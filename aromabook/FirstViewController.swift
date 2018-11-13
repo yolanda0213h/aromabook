b4193eeb04f1ac6168802f2a47f4c59f69551f0d
Yolanda H <huangyalan@yolanda-h-macbook-air.local>
2018-11-13 17:17:14 +0800

    Initial Commit

---
+++ aromabook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  aromabook
+//
+//  Created by Yolanda H. on 2018/11/11.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ aromabook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  aromabook
+//
+//  Created by Yolanda H. on 2018/11/11.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
